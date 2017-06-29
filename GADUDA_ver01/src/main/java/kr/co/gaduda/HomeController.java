c75a0ff3abb47d4eb26c4be23889a0f9a64609d6
dslc <dslc@DESKTOP-IK8TVGV>
2017-06-29 15:48:15 +0900

    되라

--- GADUDA_ver01/src/main/java/kr/co/gaduda/HomeController.java
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/HomeController.java
@@ -32,7 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-
+		
+		System.out.println("다시커밋");
 		
 		return "home";
 		
