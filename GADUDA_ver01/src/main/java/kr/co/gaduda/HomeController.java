afd1ff252d2caf6715fc2a3ba30e95b0fb6fe7ba
bit <bit@DESKTOP-G4REGIQ>
2017-06-29 14:31:42 +0900

    HANSU_test2

--- GADUDA_ver01/src/main/java/kr/co/gaduda/HomeController.java
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/HomeController.java
@@ -32,7 +32,6 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("GG");
 		
 		return "home";
 	}
