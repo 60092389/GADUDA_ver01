ed603e3e0804e1a91d3cf9b363e3ace88abe0f68
hansujung <zqx1@naver.com>
2017-07-26 14:05:13 +0900

    Create Furniture_arrScrapListVO.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/furniture_arr/vo/Furniture_arrScrapListVO.java
@@ -0,0 +1,35 @@
+package kr.co.gaduda.furniture_arr.vo;
+
+
+//2017-07-26 수정 추가
+public class Furniture_arrScrapListVO {
+	private String mem_id;
+	private String mem_nickname;
+	private String fur_arr_plan_scrap_date;
+
+	public String getMem_id() {
+		return mem_id;
+	}
+
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+
+	public String getMem_nickname() {
+		return mem_nickname;
+	}
+
+	public void setMem_nickname(String mem_nickname) {
+		this.mem_nickname = mem_nickname;
+	}
+
+	public String getFur_arr_plan_scrap_date() {
+		return fur_arr_plan_scrap_date;
+	}
+
+	public void setFur_arr_plan_scrap_date(String fur_arr_plan_scrap_date) {
+		this.fur_arr_plan_scrap_date = fur_arr_plan_scrap_date;
+	}
+	
+
+}
