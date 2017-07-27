54bd5e28a124b64eb0e841c9dadd9520cd8ae48a
hansujung <zqx1@naver.com>
2017-07-27 14:41:12 +0900

    Create MemberFurArrVO.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/member/vo/MemberFurArrVO.java
@@ -0,0 +1,101 @@
+package kr.co.gaduda.member.vo;
+
+public class MemberFurArrVO {
+	private int fur_arr_plan_no;
+	private String fur_arr_plan_img_loc;
+	private String fur_arr_create_date;
+	private String fur_arr_name;
+	private String fur_arr_contents;
+	private String mem_id;
+	private int room_no;
+	private int fur_arr_plan_good_num;
+	private int fur_arr_plan_repl_num;
+	private int fur_arr_plan_scrap_num;
+	private String fur_arr_plan_concept;
+	private String fur_arr_plan_room_kind;
+	private String fur_arr_plan_hash_tag;
+	
+	public int getFur_arr_plan_no() {
+		return fur_arr_plan_no;
+	}
+	public void setFur_arr_plan_no(int fur_arr_plan_no) {
+		this.fur_arr_plan_no = fur_arr_plan_no;
+	}
+	public String getFur_arr_plan_img_loc() {
+		return fur_arr_plan_img_loc;
+	}
+	public void setFur_arr_plan_img_loc(String fur_arr_plan_img_loc) {
+		this.fur_arr_plan_img_loc = fur_arr_plan_img_loc;
+	}
+	public String getFur_arr_create_date() {
+		return fur_arr_create_date;
+	}
+	public void setFur_arr_create_date(String fur_arr_create_date) {
+		this.fur_arr_create_date = fur_arr_create_date;
+	}
+	public String getFur_arr_name() {
+		return fur_arr_name;
+	}
+	public void setFur_arr_name(String fur_arr_name) {
+		this.fur_arr_name = fur_arr_name;
+	}
+	public String getFur_arr_contents() {
+		return fur_arr_contents;
+	}
+	public void setFur_arr_contents(String fur_arr_contents) {
+		this.fur_arr_contents = fur_arr_contents;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public int getRoom_no() {
+		return room_no;
+	}
+	public void setRoom_no(int room_no) {
+		this.room_no = room_no;
+	}
+	public int getFur_arr_plan_good_num() {
+		return fur_arr_plan_good_num;
+	}
+	public void setFur_arr_plan_good_num(int fur_arr_plan_good_num) {
+		this.fur_arr_plan_good_num = fur_arr_plan_good_num;
+	}
+	public int getFur_arr_plan_repl_num() {
+		return fur_arr_plan_repl_num;
+	}
+	public void setFur_arr_plan_repl_num(int fur_arr_plan_repl_num) {
+		this.fur_arr_plan_repl_num = fur_arr_plan_repl_num;
+	}
+	public int getFur_arr_plan_scrap_num() {
+		return fur_arr_plan_scrap_num;
+	}
+	public void setFur_arr_plan_scrap_num(int fur_arr_plan_scrap_num) {
+		this.fur_arr_plan_scrap_num = fur_arr_plan_scrap_num;
+	}
+	public String getFur_arr_plan_concept() {
+		return fur_arr_plan_concept;
+	}
+	public void setFur_arr_plan_concept(String fur_arr_plan_concept) {
+		this.fur_arr_plan_concept = fur_arr_plan_concept;
+	}
+	public String getFur_arr_plan_room_kind() {
+		return fur_arr_plan_room_kind;
+	}
+	public void setFur_arr_plan_room_kind(String fur_arr_plan_room_kind) {
+		this.fur_arr_plan_room_kind = fur_arr_plan_room_kind;
+	}
+	public String getFur_arr_plan_hash_tag() {
+		return fur_arr_plan_hash_tag;
+	}
+	public void setFur_arr_plan_hash_tag(String fur_arr_plan_hash_tag) {
+		this.fur_arr_plan_hash_tag = fur_arr_plan_hash_tag;
+	}
+	
+	
+	
+	
+	
+}
