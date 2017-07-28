88572d72910a512dd0825d224cf80ebd8fa9d5bc
hansujung <zqx1@naver.com>
2017-07-28 11:12:02 +0900

    Create FurnitureItemVO.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/furniture/vo/FurnitureItemVO.java
@@ -0,0 +1,35 @@
+package kr.co.gaduda.furniture.vo;
+
+public class FurnitureItemVO {
+	private String fur_item_no;
+	private int fur_width;
+	private int fur_height;
+	private int fur_depth;
+	public String getFur_item_no() {
+		return fur_item_no;
+	}
+	public void setFur_item_no(String fur_item_no) {
+		this.fur_item_no = fur_item_no;
+	}
+	public int getFur_width() {
+		return fur_width;
+	}
+	public void setFur_width(int fur_width) {
+		this.fur_width = fur_width;
+	}
+	public int getFur_height() {
+		return fur_height;
+	}
+	public void setFur_height(int fur_height) {
+		this.fur_height = fur_height;
+	}
+	public int getFur_depth() {
+		return fur_depth;
+	}
+	public void setFur_depth(int fur_depth) {
+		this.fur_depth = fur_depth;
+	}
+	
+	
+
+}
