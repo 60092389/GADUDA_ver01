43f75a7ecea9b4a8325394f4df7a55442f37f113
ShinJinSeop <shamao@nate.com>
2017-08-18 10:01:04 +0900

    Add files via upload

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/room/dto/DesignRoom_DTO.java
@@ -0,0 +1,56 @@
+package kr.co.gaduda.room.dto;
+
+
+public class DesignRoom_DTO {
+
+	private String fig_img_src;
+	private int fig_num;
+	private int fig_img_X;
+	private int fig_img_Y;
+	private int fig_img_width_length;
+	private int fig_img_height_length;
+	private String fig_img_state;
+	public String getFig_img_src() {
+		return fig_img_src;
+	}
+	public void setFig_img_src(String fig_img_src) {
+		this.fig_img_src = fig_img_src;
+	}
+	public int getFig_num() {
+		return fig_num;
+	}
+	public void setFig_num(int fig_num) {
+		this.fig_num = fig_num;
+	}
+	public int getFig_img_X() {
+		return fig_img_X;
+	}
+	public void setFig_img_X(int fig_img_X) {
+		this.fig_img_X = fig_img_X;
+	}
+	public int getFig_img_Y() {
+		return fig_img_Y;
+	}
+	public void setFig_img_Y(int fig_img_Y) {
+		this.fig_img_Y = fig_img_Y;
+	}
+	public int getFig_img_width_length() {
+		return fig_img_width_length;
+	}
+	public void setFig_img_width_length(int fig_img_width_length) {
+		this.fig_img_width_length = fig_img_width_length;
+	}
+	public int getFig_img_height_length() {
+		return fig_img_height_length;
+	}
+	public void setFig_img_height_length(int fig_img_height_length) {
+		this.fig_img_height_length = fig_img_height_length;
+	}
+	public String getFig_img_state() {
+		return fig_img_state;
+	}
+	public void setFig_img_state(String fig_img_state) {
+		this.fig_img_state = fig_img_state;
+	}
+
+}
