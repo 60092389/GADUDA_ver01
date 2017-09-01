f6c5e4bb1c6af05bddc8af0e7a9d8fd2cafc3427
Park Jun Buyng <artroy1990@naver.com>
2017-09-01 12:49:34 +0900

    Create Scrap_No.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/room/vo/Scrap_No.java
@@ -0,0 +1,17 @@
+package kr.co.gaduda.room.vo;
+
+public class Scrap_No {
+	
+	private int fur_no;
+
+	public int getFur_no() {
+		return fur_no;
+	}
+
+	public void setFur_no(int fur_no) {
+		this.fur_no = fur_no;
+	}
+	
+	
+
+}
