fcd02fc9120fc76a972e9d0ea263a9c13dea506f
Park Jun Buyng <artroy1990@naver.com>
2017-09-01 12:48:23 +0900

    Create DesignRoom_Update_DTO.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/room/dto/DesignRoom_Update_DTO.java
@@ -0,0 +1,30 @@
+package kr.co.gaduda.room.dto;
+
+public class DesignRoom_Update_DTO {
+	private int updata_num;
+	private String designRoom_name;
+	private String designroom_info;
+	public int getUpdata_num() {
+		return updata_num;
+	}
+	public void setUpdata_num(int updata_num) {
+		this.updata_num = updata_num;
+	}
+	public String getDesignRoom_name() {
+		return designRoom_name;
+	}
+	public void setDesignRoom_name(String designRoom_name) {
+		this.designRoom_name = designRoom_name;
+	}
+	public String getDesignroom_info() {
+		return designroom_info;
+	}
+	public void setDesignroom_info(String designroom_info) {
+		this.designroom_info = designroom_info;
+	}
+	
+
+	
+	
+
+}
