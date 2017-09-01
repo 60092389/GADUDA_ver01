8fb17e7f75079ad525d3b9d6ba99097253f8c879
Park Jun Buyng <artroy1990@naver.com>
2017-09-01 12:49:01 +0900

    Create Furniture_Up_Room_VO.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/room/vo/Furniture_Up_Room_VO.java
@@ -0,0 +1,33 @@
+package kr.co.gaduda.room.vo;
+
+public class Furniture_Up_Room_VO {
+	
+	private int fur_arr_plan_no;
+	private int room_no;
+	private String room_img_src;
+	public int getFur_arr_plan_no() {
+		return fur_arr_plan_no;
+	}
+	public void setFur_arr_plan_no(int fur_arr_plan_no) {
+		this.fur_arr_plan_no = fur_arr_plan_no;
+	}
+	public int getRoom_no() {
+		return room_no;
+	}
+	public void setRoom_no(int room_no) {
+		this.room_no = room_no;
+	}
+	public String getRoom_img_src() {
+		return room_img_src;
+	}
+	public void setRoom_img_src(String room_img_src) {
+		this.room_img_src = room_img_src;
+	}
+
+	
+	
+	
+	
+	
+
+}
