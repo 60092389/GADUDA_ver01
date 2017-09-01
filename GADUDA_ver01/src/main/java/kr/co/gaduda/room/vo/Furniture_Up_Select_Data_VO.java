d4cebec1817e175e24461e7b8a358589abbab60f
Park Jun Buyng <artroy1990@naver.com>
2017-09-01 12:49:21 +0900

    Create Furniture_Up_Select_Data_VO.java

---
+++ GADUDA_ver01/src/main/java/kr/co/gaduda/room/vo/Furniture_Up_Select_Data_VO.java
@@ -0,0 +1,60 @@
+package kr.co.gaduda.room.vo;
+
+public class Furniture_Up_Select_Data_VO {
+	
+	private int arr_fur_no;
+	private int fur_no;
+	private int fur_arr_plan_no;
+	private int arr_fur_X_value;
+	private int arr_fur_Y_value;
+	private String arr_fur_state;
+	private String fur_arr_basic_img_loc;
+	
+	public int getFur_no() {
+		return fur_no;
+	}
+	public void setFur_no(int fur_no) {
+		this.fur_no = fur_no;
+	}
+	public int getArr_fur_no() {
+		return arr_fur_no;
+	}
+	public void setArr_fur_no(int arr_fur_no) {
+		this.arr_fur_no = arr_fur_no;
+	}
+	public int getFur_arr_plan_no() {
+		return fur_arr_plan_no;
+	}
+	public void setFur_arr_plan_no(int fur_arr_plan_no) {
+		this.fur_arr_plan_no = fur_arr_plan_no;
+	}
+	public int getArr_fur_X_value() {
+		return arr_fur_X_value;
+	}
+	public void setArr_fur_X_value(int arr_fur_X_value) {
+		this.arr_fur_X_value = arr_fur_X_value;
+	}
+	public int getArr_fur_Y_value() {
+		return arr_fur_Y_value;
+	}
+	public void setArr_fur_Y_value(int arr_fur_Y_value) {
+		this.arr_fur_Y_value = arr_fur_Y_value;
+	}
+	public String getArr_fur_state() {
+		return arr_fur_state;
+	}
+	public void setArr_fur_state(String arr_fur_state) {
+		this.arr_fur_state = arr_fur_state;
+	}
+	public String getFur_arr_basic_img_loc() {
+		return fur_arr_basic_img_loc;
+	}
+	public void setFur_arr_basic_img_loc(String fur_arr_basic_img_loc) {
+		this.fur_arr_basic_img_loc = fur_arr_basic_img_loc;
+	}
+	
+	
+	
+	
+
+}
